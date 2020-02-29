8cf5aab6694f65b90988f2054f7b83a8cdc7190f
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2020-02-29 18:10:57 +0200

    Update settings.py

--- Backend/Milano/settings.py
+++ Backend/Milano/settings.py
@@ -153,3 +153,6 @@ AUTH_USER_MODEL = 'Account.CustomUser'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
