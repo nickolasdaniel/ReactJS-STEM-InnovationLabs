a068781ce743cc7e8e0b24228d06f02f55c17d2d
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2020-03-01 19:29:10 +0200

    Update Homepage.js

--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Redirect, Link} from 'react-router-dom';
+import axios from 'axios';
 
 class Homepage extends React.Component {
     constructor(props) {
@@ -7,21 +8,66 @@ class Homepage extends React.Component {
 
         this.state = {
             isLoggedOut: false,
-            username:"",
+            username:'',
+            pk: null,
+            created: '',
+            is_doctor: null,
+            is_pacient: null
         }
         
         setTimeout(function(){
             const user = localStorage.getItem('user');
-            console.log("HOMEPAGE", user);
+            const pk = localStorage.getItem('pk');
+            const is_pacient = localStorage.getItem('is_pacient');
+            const is_doctor = localStorage.getItem('is_doctor');
+            // console.log('TIMEOUT PK' + pk);
+            // console.log("HOMEPAGE", user);
             this.setState({username:user});
+            this.setState({pk: pk});
+            this.setState({is_doctor: is_doctor})
+            this.setState({is_pacient: is_pacient})
+            this.checkIfProfileIsCreated();
         }.bind(this), 0);
         
     }
+
+    checkIfProfileIsCreated = () => {
+        
+        // console.log(pk)
+        const URL = `http://192.168.100.11:8081/profile_pacient/${this.state.pk}/`;
+        console.log(URL)
+        axios.get(URL)
+            .then((response) => {
+                if(response.data){
+                    // return <Redirect to='/pacient_profile' />
+                    this.setState({created: true})
+                    console.log(this.state)
+                    console.log("FACE REQUEST")
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                console.log("NU ESTE PROFIL")
+            })
+    }
+
+    linkToDocORPac = () => {
+        if(this.state.is_pacient === 'true') {
+            return this.state.created ? '/pacient_profile' : '/profile'
+        } else if(this.state.is_doctor === 'true') {
+            return this.state.created ? '/doctor_profile_page' : '/doctor_profile_create'
+        }
+    }
+    // componentDidMount() {
+    //     this.checkIfProfileIsCreated();
+    // }
 
     logoutUser = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('user');
         localStorage.removeItem('pk')
+        localStorage.removeItem('is_pacient')
+        localStorage.removeItem('is_doctor')
         this.setState({isLoggedOut: true})
     }
 
@@ -37,8 +83,7 @@ class Homepage extends React.Component {
                     </div>
                     <ul className="nav navbar-nav">
                         <li><a href="#">Home</a></li>
-                        <li><Link to='/profile'>Profile</Link></li>
-
+                        <li><Link to={this.linkToDocORPac}>Profile</Link></li> 
                     </ul>
                 </div>
                 </nav>  
